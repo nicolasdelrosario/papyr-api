ade3f15cc20c56522340e578f90bd1c6a999ab80
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-19 14:47:01 -0500

    refactor: :recycle: Rename UserName to UserUsername for better domain semantics

--- src/modules/users/application/GetUserByUsername.ts
+++ src/modules/users/application/GetUserByUsername.ts
@@ -3,13 +3,13 @@ import type { UserRepository } from "@users/domain/repository/UserRepository";
 
 import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
 import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
-import { UserName } from "@users/domain/value-objects/UserName";
+import { UserUsername } from "@users/domain/value-objects/UserUsername";
 
 export class GetUserByUsername {
   constructor(private readonly repository: UserRepository) {}
 
   async execute(username: string): Promise<User> {
-    const user = await this.repository.findByUsername(new UserName(username));
+    const user = await this.repository.findByUsername(new UserUsername(username));
 
     if (!user) throw new UserNotFound("User not found.");
 
