5a33ba4bef2b95fc7c6228353369339eb17d0b8f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-19 17:18:45 -0500

    feat: :sparkles: Add RemoveUser class with soft-delete functionality

---
+++ src/modules/users/application/RemoveUser.ts
@@ -0,0 +1,21 @@
+import { UserActive } from "@users/domain/exceptions/UserActive";
+import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
+import type { UserRepository } from "@users/domain/repository/UserRepository";
+import { UserId } from "@users/domain/value-objects/UserId";
+import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
+
+export class RemoveUser {
+  constructor(private readonly repository: UserRepository) {}
+
+  async execute(id: string): Promise<void> {
+    const user = await this.repository.findById(new UserId(id));
+
+    if (!user) throw new UserNotFound(`User with id ${id} not found`);
+
+    if (user.deletedAt.value !== null)
+      throw new UserNotActive(`User with id ${id} is already deleted`);
+
+    user.markAsDeleted();
+    await this.repository.edit(user);
+  }
+}
