abe5bef16a29d2ad2425bdd27bcd980127d8cff3
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-19 12:18:04 -0500

    feat: :sparkles: Add User domain model class

---
+++ src/modules/users/domain/model/User.ts
@@ -0,0 +1,69 @@
+import type { UserId } from "@users/domain/value-objects/UserId";
+import type { UserName } from "@users/domain/value-objects/UserName";
+import type { UserEmail } from "@users/domain/value-objects/UserEmail";
+import type { UserPassword } from "@users/domain/value-objects/UserPassword";
+import type { UserAvatarUrl } from "@users/domain/value-objects/UserAvatarUrl";
+import type { UserCreatedAt } from "@users/domain/value-objects/UserCreatedAt";
+import type { UserUpdatedAt } from "@users/domain/value-objects/UserUpdatedAt";
+import type { UserDeletedAt } from "@users/domain/value-objects/UserDeletedAt";
+
+export class User {
+  id: UserId;
+  name: UserName;
+  username: UserName;
+  email: UserEmail;
+  password: UserPassword;
+  avatarUrl: UserAvatarUrl;
+  createdAt: UserCreatedAt;
+  updatedAt: UserUpdatedAt;
+  deletedAt: UserDeletedAt;
+
+  constructor(
+    id: UserId,
+    name: UserName,
+    username: UserName,
+    email: UserEmail,
+    password: UserPassword,
+    avatarUrl: UserAvatarUrl,
+    createdAt: UserCreatedAt,
+    updatedAt: UserUpdatedAt,
+    deletedAt: UserDeletedAt,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.avatarUrl = avatarUrl;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+
+  public toPrimitives() {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      username: this.username.value,
+      email: this.email.value,
+      password: this.password.value,
+      avatarUrl: this.avatarUrl.value,
+      createdAt: this.createdAt.value,
+      updatedAt: this.updatedAt.value,
+      deletedAt: this.deletedAt.value,
+    };
+  }
+
+  public toSafePrimitives() {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      username: this.username.value,
+      email: this.email.value,
+      avatarUrl: this.avatarUrl.value,
+      createdAt: this.createdAt.value,
+      updatedAt: this.updatedAt.value,
+      deletedAt: this.deletedAt.value,
+    };
+  }
+}
