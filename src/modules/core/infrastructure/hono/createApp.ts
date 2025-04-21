8e3db77f8c23428b274f1a69acb56d240ad273c8
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-20 23:46:26 -0500

    feat :sparkles: Add servicesMiddleware to app middleware chain

--- src/modules/core/infrastructure/hono/createApp.ts
+++ src/modules/core/infrastructure/hono/createApp.ts
@@ -1,6 +1,7 @@
 import type { App } from "@/modules/core/infrastructure/hono/types/App";
 import { notFound } from "@core/infrastructure/hono/middlewares/notFound";
 import { onError } from "@core/infrastructure/hono/middlewares/onError";
+import { servicesMiddleware } from "@core/infrastructure/hono/middlewares/servicesMiddleware";
 import { validateEnvironment } from "@core/infrastructure/hono/middlewares/validateEnvironment";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
@@ -20,6 +21,7 @@ export const createApp = () => {
   app.onError(onError);
 
   app.use("*", validateEnvironment);
+  app.use("*", servicesMiddleware);
 
   return app;
 };
