7d7285d02dd075cf593efa54e25ff3a0e9cd718c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-21 18:35:04 -0500

    feat :sparkles: Register routes with the application

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { createApp } from "@core/infrastructure/hono/createApp";
+import { registerRoutes } from "@core/infrastructure/hono/routes/registerRoutes";
 
 const app = createApp();
+
+registerRoutes(app);
 
 export default app;
