1c0f5d30bf2e3d363561c365099ee5e662b75ebe
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-21 18:01:04 -0500

    feat: :sparkles: Add RemoveUserController for user soft deletion endpoint

---
+++ src/modules/users/infrastructure/controllers/RemoveUserController.ts
@@ -0,0 +1,29 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
+import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class RemoveUserController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.users.remove.execute(id);
+
+      return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof UserNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof UserNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
