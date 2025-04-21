e648d5c73853dc17cadbfdad41018afd40aaf6a5
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-21 17:47:09 -0500

    feat: :sparkles: Add DeleteUserController to handle user deletion API endpoint

---
+++ src/modules/users/infrastructure/controllers/DeleteUserController.ts
@@ -0,0 +1,28 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+import { UserActive } from "../../domain/exceptions/UserActive";
+import { UserNotFound } from "../../domain/exceptions/UserNotFound";
+
+export class DeleteUserController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.users.delete.execute(id);
+
+      return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof UserNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof UserActive) return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
