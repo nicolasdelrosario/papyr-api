92b74308fd3a2d9f63b5be783d48cd48ebcb65ae
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-21 18:01:24 -0500

    feat :sparkles: Add RestoreUserController implementation

---
+++ src/modules/users/infrastructure/controllers/RestoreUserController.ts
@@ -0,0 +1,28 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { UserActive } from "@users/domain/exceptions/UserActive";
+import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class RestoreUserController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.users.restore.execute(id);
+
+      return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof UserNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof UserActive) return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
