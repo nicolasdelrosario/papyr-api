507f2baf7bdd52fe3ef1ee700d63640537d77f3c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-20 23:46:03 -0500

    feat :sparkles: Add services to Variables type in Context.ts

--- src/modules/core/infrastructure/hono/types/Context.ts
+++ src/modules/core/infrastructure/hono/types/Context.ts
@@ -1,5 +1,7 @@
+import type { Services } from "@core/infrastructure/Services";
 import type { Env } from "@core/infrastructure/config/environment";
 
 export type Variables = {
   env: Env;
+  services: Services;
 };
