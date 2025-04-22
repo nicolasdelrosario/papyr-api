e5795ef5281746a76a7c7718b37e30db404aa42e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-22 14:58:09 -0500

    feat: :sparkles: Add authentication middleware for protected routes

---
+++ src/modules/core/infrastructure/hono/middlewares/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { InvalidToken } from "@auth/domain/exceptions/InvalidToken";
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { App } from "@core/infrastructure/hono/types/App";
+import type { Context, MiddlewareHandler, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
+
+const publicRoutes = ["/api/v1/login", "/api/v1/signup"];
+
+export const authMiddleware: MiddlewareHandler = async (c: Context<App>, next: Next) => {
+  const route = c.req.path;
+
+  if (publicRoutes.includes(route)) return await next();
+
+  const token = getCookie(c, "token");
+
+  if (!token)
+    return c.json(
+      {
+        data: null,
+        message: HttpStatusPhrases.UNAUTHORIZED,
+      },
+      HttpStatusCodes.UNAUTHORIZED,
+    );
+
+  const payload = await verify(token, c.env.JWT_SECRET);
+
+  if (!payload) throw new InvalidToken("Invalid token");
+
+  c.set("jwtPayload", payload);
+
+  return await next();
+};
