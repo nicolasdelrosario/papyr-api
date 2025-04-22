d9215dc6e864ac356ebee8979f8bee41dba2defb
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-22 14:49:25 -0500

    refactor: :recycle: Refactor user controller imports and change signup endpoint

--- src/modules/users/infrastructure/routes/honoUserRoutes.ts
+++ src/modules/users/infrastructure/routes/honoUserRoutes.ts
@@ -1,14 +1,14 @@
 import type { App } from "@/modules/core/infrastructure/hono/types/App";
 import { zValidator } from "@hono/zod-validator";
+import { DeleteUserController } from "@users/infrastructure/controllers/DeleteUserController";
+import { EditUserController } from "@users/infrastructure/controllers/EditUserController";
 import { GetUserByIdController } from "@users/infrastructure/controllers/GetUserByIdController";
 import { GetUsersController } from "@users/infrastructure/controllers/GetUsersController";
 import { RegisterUserController } from "@users/infrastructure/controllers/RegisterUserController";
+import { RemoveUserController } from "@users/infrastructure/controllers/RemoveUserController";
+import { RestoreUserController } from "@users/infrastructure/controllers/RestoreUserController";
 import { userEditSchema, userIdParamSchema, userRegisterSchema } from "@users/infrastructure/schemas/zodUserSchema";
 import type { Hono } from "hono";
-import { DeleteUserController } from "../controllers/DeleteUserController";
-import { EditUserController } from "../controllers/EditUserController";
-import { RemoveUserController } from "../controllers/RemoveUserController";
-import { RestoreUserController } from "../controllers/RestoreUserController";
 
 const getUsers = new GetUsersController();
 const getUserById = new GetUserByIdController();
@@ -21,7 +21,7 @@ const deleteUser = new DeleteUserController();
 export const register = (app: Hono<App>): void => {
   app.get("/users", getUsers.handle);
   app.get("/users/:id", zValidator("param", userIdParamSchema), getUserById.handle);
-  app.post("/users", zValidator("json", userRegisterSchema), registerUser.handle);
+  app.post("/signup", zValidator("json", userRegisterSchema), registerUser.handle);
   app.put("/users/:id", zValidator("param", userIdParamSchema), zValidator("json", userEditSchema), editUser.handle);
   app.patch("/users/:id/remove", zValidator("param", userIdParamSchema), removeUser.handle);
   app.patch("/users/:id/restore", zValidator("param", userIdParamSchema), restoreUser.handle);
