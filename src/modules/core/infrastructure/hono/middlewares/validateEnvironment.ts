9b76ebba083f0ac0753837d42b9bdaca0a880f2e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-22 14:51:28 -0500

    feat: :sparkles: Add type annotations to validateEnvironment middleware

--- src/modules/core/infrastructure/hono/middlewares/validateEnvironment.ts
+++ src/modules/core/infrastructure/hono/middlewares/validateEnvironment.ts
@@ -1,7 +1,8 @@
 import { type Env, parseValidatedEnv } from "@core/infrastructure/config/environment";
-import type { MiddlewareHandler } from "hono";
+import type { App } from "@core/infrastructure/hono/types/App";
+import type { Context, MiddlewareHandler, Next } from "hono";
 
-export const validateEnvironment: MiddlewareHandler = async (c, next) => {
+export const validateEnvironment: MiddlewareHandler = async (c: Context<App>, next: Next) => {
   const envConfig: Env = parseValidatedEnv(c.env);
 
   c.set("env", envConfig);
