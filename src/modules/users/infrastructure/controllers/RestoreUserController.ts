401b413111a9a9daee8c054fae0d23655f302c9d
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 15:22:46 -0500

    refactor: :recycle: Update exception imports in RestoreUserController for consistency

--- src/modules/users/infrastructure/controllers/RestoreUserController.ts
+++ src/modules/users/infrastructure/controllers/RestoreUserController.ts
@@ -2,8 +2,8 @@ import * as HttpStatusCodes from "@core/common/httpStatusCodes";
 import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
 import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
 import type { App } from "@core/infrastructure/hono/types/App";
-import { UserActive } from "@users/domain/exceptions/UserActive";
-import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
+import { UserIsActive } from "@users/domain/exceptions/UserIsActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
 import type { Context, TypedResponse } from "hono";
 import type { StatusCode } from "hono/utils/http-status";
 
@@ -15,12 +15,19 @@ export class RestoreUserController implements Controller {
 
       await services.users.restore.execute(id);
 
-      return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
+      return c.json(
+        {
+          data: null,
+          message: HttpStatusPhrases.OK,
+        },
+        HttpStatusCodes.OK,
+      );
     } catch (error) {
-      if (error instanceof UserNotFound)
+      if (error instanceof UserWasNotFound)
         return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
 
-      if (error instanceof UserActive) return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+      if (error instanceof UserIsActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.CONFLICT);
 
       throw error;
     }
