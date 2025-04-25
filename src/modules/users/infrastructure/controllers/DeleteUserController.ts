87e35edc1f6a9bd0561150c2b8302e7f92512fee
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 15:23:32 -0500

    refactor: :recycle: Update exception handling in DeleteUserController for consistency

--- src/modules/users/infrastructure/controllers/DeleteUserController.ts
+++ src/modules/users/infrastructure/controllers/DeleteUserController.ts
@@ -2,10 +2,10 @@ import * as HttpStatusCodes from "@core/common/httpStatusCodes";
 import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
 import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
 import type { App } from "@core/infrastructure/hono/types/App";
+import { UserIsActive } from "@users/domain/exceptions/UserIsActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
 import type { Context, TypedResponse } from "hono";
 import type { StatusCode } from "hono/utils/http-status";
-import { UserActive } from "../../domain/exceptions/UserActive";
-import { UserNotFound } from "../../domain/exceptions/UserNotFound";
 
 export class DeleteUserController implements Controller {
   async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
@@ -17,10 +17,11 @@ export class DeleteUserController implements Controller {
 
       return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
     } catch (error) {
-      if (error instanceof UserNotFound)
+      if (error instanceof UserWasNotFound)
         return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
 
-      if (error instanceof UserActive) return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+      if (error instanceof UserIsActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
 
       throw error;
     }
