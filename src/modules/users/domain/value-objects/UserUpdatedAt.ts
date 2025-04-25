08b5f445674550bb2084272dfb78a8bd6c5041b7
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 12:36:35 -0500

    refactor: :recycle: Enhance validation in UserUpdatedAt class to ensure valid Date instance and improve error handling

--- src/modules/users/domain/value-objects/UserUpdatedAt.ts
+++ src/modules/users/domain/value-objects/UserUpdatedAt.ts
@@ -7,6 +7,10 @@ export class UserUpdatedAt {
   }
 
   private validate(date: Date): void {
-    if (date > new Date()) throw new Error("UpdatedAt must be in the past");
+    if (Number.isNaN(date.getTime()))
+      throw new Error("UpdatedAt date must be a valid Date instance");
+
+    const now = new Date();
+    if (date > now) throw new Error("UpdatedAt must be in the past");
   }
 }
