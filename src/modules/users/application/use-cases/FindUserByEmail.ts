27a461fb977d321b0b2d5105dc9eea06e9f2794f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 14:27:27 -0500

    move: :truck: move find user by email use-case

--- src/modules/users/application/use-cases/FindUserByEmail.ts
+++ src/modules/users/application/use-cases/FindUserByEmail.ts
@@ -1,19 +1,18 @@
+import { UserIsNotActive } from "@users/domain/exceptions/UserIsNotActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
 import type { User } from "@users/domain/model/User";
 import type { UserRepository } from "@users/domain/repository/UserRepository";
 import { UserEmail } from "@users/domain/value-objects/UserEmail";
 
-import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
-import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
-
-export class GetUserByEmail {
+export class FindUserByEmail {
   constructor(private readonly repository: UserRepository) {}
 
   async execute(email: string): Promise<User> {
     const user = await this.repository.findByEmail(new UserEmail(email));
 
-    if (!user) throw new UserNotFound("User not found.");
+    if (!user) throw new UserWasNotFound("User was not found.");
 
-    if (user.deletedAt.value !== null) throw new UserNotActive("User is not active.");
+    if (!user.isActive()) throw new UserIsNotActive("User is not active.");
 
     return user;
   }
