68af62f048f9df1ca0d03c1875eff792515e5f59
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 12:35:02 -0500

    refactor: :recycle: Update UserUsername class to improve username validation logic and ensure proper input handling

--- src/modules/users/domain/value-objects/UserUsername.ts
+++ src/modules/users/domain/value-objects/UserUsername.ts
@@ -1,7 +1,17 @@
 export class UserUsername {
   constructor(public readonly value: string) {
-    if (!/^[a-zA-Z0-9_]{3,30}$/.test(value)) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(username: string): void {
+    if (!username || typeof username !== "string")
+      throw new Error("Username must be a string.");
+
+    if (username.trim().length < 3 || username.trim().length > 20)
+      throw new Error("Username must be between 3 and 20 characters.");
+
+    if (!/^[a-zA-Z0-9_]{3,20}$/.test(username))
       throw new Error("Invalid username.");
-    }
   }
 }
