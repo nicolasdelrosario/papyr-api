d732bf1044d7cfe1364fd4620ea6ef5273a26c7c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 14:20:33 -0500

    chore: :truck: move find user by id use-case

--- src/modules/users/application/use-cases/FindUserById.ts
+++ src/modules/users/application/use-cases/FindUserById.ts
@@ -1,19 +1,18 @@
+import { UserIsNotActive } from "@users/domain/exceptions/UserIsNotActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
 import type { User } from "@users/domain/model/User";
 import type { UserRepository } from "@users/domain/repository/UserRepository";
 import { UserId } from "@users/domain/value-objects/UserId";
 
-import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
-import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
-
-export class GetUserById {
+export class FindUserById {
   constructor(private readonly repository: UserRepository) {}
 
   async execute(id: string): Promise<User> {
     const user = await this.repository.findById(new UserId(id));
 
-    if (!user) throw new UserNotFound("User not found.");
+    if (!user) throw new UserWasNotFound("User was not found.");
 
-    if (user.deletedAt.value !== null) throw new UserNotActive("User is not active.");
+    if (!user.isActive()) throw new UserIsNotActive("User is not active.");
 
     return user;
   }
