814d05b361649e2da3e17d354f1061007894a9a8
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 15:23:08 -0500

    feat: :sparkles: Implement user retrieval controllers by email, id, and username with error handling

---
+++ src/modules/users/infrastructure/controllers/FindUserByIdController.ts
@@ -0,0 +1,35 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { UserIsNotActive } from "@users/domain/exceptions/UserIsNotActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class FindUserByIdController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      const user = await services.users.findById.execute(id);
+
+      return c.json(
+        {
+          data: user.toSafePrimitives(),
+          message: HttpStatusPhrases.OK,
+        },
+        HttpStatusCodes.OK,
+      );
+    } catch (error) {
+      if (error instanceof UserWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof UserIsNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
