afad5a6474346ffa9782cc35a7a461053b3986d2
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 12:48:03 -0500

    refactor: :recycle: Update import statement for UserDeletedAt and enhance user state management methods

--- src/modules/users/domain/model/User.ts
+++ src/modules/users/domain/model/User.ts
@@ -1,6 +1,6 @@
 import type { UserAvatarUrl } from "@users/domain/value-objects/UserAvatarUrl";
 import type { UserCreatedAt } from "@users/domain/value-objects/UserCreatedAt";
-import type { UserDeletedAt } from "@users/domain/value-objects/UserDeletedAt";
+import { UserDeletedAt } from "@users/domain/value-objects/UserDeletedAt";
 import type { UserEmail } from "@users/domain/value-objects/UserEmail";
 import type { UserId } from "@users/domain/value-objects/UserId";
 import type { UserName } from "@users/domain/value-objects/UserName";
@@ -66,5 +66,17 @@ export class User {
       updatedAt: this.updatedAt.value,
       deletedAt: this.deletedAt.value,
     };
+  }
+
+  isActive(): boolean {
+    return this.deletedAt.value === null;
+  }
+
+  remove(): void {
+    this.deletedAt = new UserDeletedAt(new Date());
+  }
+
+  restore(): void {
+    this.deletedAt = new UserDeletedAt(null);
   }
 }
