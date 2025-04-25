e2dc265ee36e24f3c20fc539dc801e09913bf09f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-25 15:22:28 -0500

    feat: :sparkles: Add SoftDeleteUserController for handling user soft deletion

---
+++ src/modules/users/infrastructure/controllers/SoftDeleteUserController.ts
@@ -0,0 +1,35 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+import { UserIsNotActive } from "../../domain/exceptions/UserIsNotActive";
+
+export class SoftDeleteUserController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.users.softDelete.execute(id);
+
+      return c.json(
+        {
+          data: null,
+          message: HttpStatusPhrases.OK,
+        },
+        HttpStatusCodes.OK,
+      );
+    } catch (error) {
+      if (error instanceof UserWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof UserIsNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
