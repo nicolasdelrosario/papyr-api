353fa003f0f5f52d6d51320b9a9fe6b45fbf50a5
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 15:13:19 -0500

    feat: :sparkles: Add AuthorUpdatedAt value object with date validation

---
+++ src/modules/authors/domain/value-objects/AuthorUpdatedAt.ts
@@ -0,0 +1,15 @@
+export class AuthorUpdatedAt {
+  public readonly value: Date;
+
+  constructor(value: Date) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(date: Date): void {
+    if (Number.isNaN(date.getTime())) throw new Error("UpdatedAt date must be a valid Date instance");
+
+    const now = new Date();
+    if (date > now) throw new Error("UpdatedAt must be in the past");
+  }
+}
