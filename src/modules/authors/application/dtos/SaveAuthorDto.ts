e703d38ecad089f01f57c38b29ea1aa241b0b691
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 15:44:21 -0500

    feat: :sparkles: Add SaveAuthorDto interface for author management

---
+++ src/modules/authors/application/dtos/SaveAuthorDto.ts
@@ -0,0 +1,12 @@
+export interface SaveAuthorDto {
+  id: string;
+  name: string;
+  biography: string | null;
+  birth_date: Date;
+  death_date: Date | null;
+  nationality: string | null;
+  photo_url: string | null;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date | null;
+}
