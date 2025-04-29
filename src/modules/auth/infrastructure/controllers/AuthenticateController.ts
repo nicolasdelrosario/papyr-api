b4eb8ff44e4cd519512a6389b4adced26893eb33
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 12:06:33 -0500

    Update authentication controller with improved naming and responses

--- src/modules/auth/infrastructure/controllers/AuthenticateController.ts
+++ src/modules/auth/infrastructure/controllers/AuthenticateController.ts
@@ -3,8 +3,8 @@ import * as HttpStatusCodes from "@core/common/httpStatusCodes";
 import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
 import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
 import type { App } from "@core/infrastructure/hono/types/App";
-import { UserNotActive } from "@users/domain/exceptions/UserNotActive";
-import { UserNotFound } from "@users/domain/exceptions/UserNotFound";
+import { UserIsNotActive } from "@users/domain/exceptions/UserIsNotActive";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
 import type { Context, TypedResponse } from "hono";
 import { setCookie } from "hono/cookie";
 import type { StatusCode } from "hono/utils/http-status";
@@ -15,9 +15,9 @@ export class AuthenticateController implements Controller {
       const services = c.get("services");
       const { email, password } = await c.req.json();
 
-      const token = await services.auth.authenticate.execute(email, password);
+      const user = await services.auth.authenticate.execute(email, password);
 
-      setCookie(c, "token", token, {
+      setCookie(c, "token", user.token, {
         path: "/",
         httpOnly: true,
         secure: c.env.ENVIRONMENT === "production",
@@ -25,16 +25,22 @@ export class AuthenticateController implements Controller {
         maxAge: 60 * 60 * 7,
       });
 
-      return c.json({ data: null, message: HttpStatusPhrases.OK }, HttpStatusCodes.OK);
+      return c.json(
+        {
+          data: { id: user.id, name: user.name, email: user.email },
+          message: HttpStatusPhrases.OK,
+        },
+        HttpStatusCodes.OK,
+      );
     } catch (error) {
-      if (error instanceof UserNotFound)
-        return c.json({ data: null, message: HttpStatusPhrases.NOT_FOUND }, HttpStatusCodes.NOT_FOUND);
+      if (error instanceof UserWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
 
-      if (error instanceof UserNotActive)
-        return c.json({ data: null, message: HttpStatusPhrases.FORBIDDEN }, HttpStatusCodes.FORBIDDEN);
+      if (error instanceof UserIsNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
 
       if (error instanceof InvalidCredentials)
-        return c.json({ data: null, message: HttpStatusPhrases.UNAUTHORIZED }, HttpStatusCodes.UNAUTHORIZED);
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.UNAUTHORIZED);
 
       throw error;
     }
