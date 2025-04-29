4264c1ce332a76afb5010ecddbf7b957f9dfc091
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 12:33:28 -0500

    refactor: :recycle: Refactor user routes with consistent naming and API design

--- src/modules/users/infrastructure/routes/honoUserRoutes.ts
+++ src/modules/users/infrastructure/routes/honoUserRoutes.ts
@@ -1,29 +1,29 @@
-import type { App } from "@/modules/core/infrastructure/hono/types/App";
+import type { App } from "@core/infrastructure/hono/types/App";
 import { zValidator } from "@hono/zod-validator";
 import { DeleteUserController } from "@users/infrastructure/controllers/DeleteUserController";
-import { EditUserController } from "@users/infrastructure/controllers/EditUserController";
-import { GetUserByIdController } from "@users/infrastructure/controllers/GetUserByIdController";
-import { GetUsersController } from "@users/infrastructure/controllers/GetUsersController";
-import { RegisterUserController } from "@users/infrastructure/controllers/RegisterUserController";
-import { RemoveUserController } from "@users/infrastructure/controllers/RemoveUserController";
+import { FindUserByIdController } from "@users/infrastructure/controllers/FindUserByIdController";
+import { ListUsersController } from "@users/infrastructure/controllers/ListUsersController";
 import { RestoreUserController } from "@users/infrastructure/controllers/RestoreUserController";
-import { userEditSchema, userIdParamSchema, userRegisterSchema } from "@users/infrastructure/schemas/zodUserSchema";
+import { SaveUserController } from "@users/infrastructure/controllers/SaveUserController";
+import { SoftDeleteUserController } from "@users/infrastructure/controllers/SoftDeleteUserController";
+import { userIdParamSchema, userSaveSchema } from "@users/infrastructure/schemas/zodUserSchema";
 import type { Hono } from "hono";
 
-const getUsers = new GetUsersController();
-const getUserById = new GetUserByIdController();
-const registerUser = new RegisterUserController();
-const editUser = new EditUserController();
-const removeUser = new RemoveUserController();
-const restoreUser = new RestoreUserController();
+const list = new ListUsersController();
+const findById = new FindUserByIdController();
+const save = new SaveUserController();
 const deleteUser = new DeleteUserController();
+const restore = new RestoreUserController();
+const softDelete = new SoftDeleteUserController();
+
+const idValidation = zValidator("param", userIdParamSchema);
+const saveValidation = zValidator("json", userSaveSchema);
 
 export const register = (app: Hono<App>): void => {
-  app.get("/users", getUsers.handle);
-  app.get("/users/:id", zValidator("param", userIdParamSchema), getUserById.handle);
-  app.post("/signup", zValidator("json", userRegisterSchema), registerUser.handle);
-  app.put("/users/:id", zValidator("param", userIdParamSchema), zValidator("json", userEditSchema), editUser.handle);
-  app.patch("/users/:id/remove", zValidator("param", userIdParamSchema), removeUser.handle);
-  app.patch("/users/:id/restore", zValidator("param", userIdParamSchema), restoreUser.handle);
-  app.delete("/users/:id", zValidator("param", userIdParamSchema), deleteUser.handle);
+  app.get("/users", list.handle);
+  app.get("/users/:id", idValidation, findById.handle);
+  app.post("/users", saveValidation, save.handle);
+  app.delete("/users/:id", idValidation, deleteUser.handle);
+  app.post("/users/:id/restore", idValidation, restore.handle);
+  app.post("/users/:id/soft-delete", idValidation, softDelete.handle);
 };
