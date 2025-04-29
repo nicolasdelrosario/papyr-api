c659fbb54365672dd3b30d3e529e0b90993f7d6e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 16:22:37 -0500

    feat: :sparkles: Add FindPublisherById use case implementation

---
+++ src/modules/publishers/application/use-cases/FindPublisherById.ts
@@ -0,0 +1,20 @@
+import { PublisherIsNotActive } from "@publishers/domain/exceptions/PublisherIsNotActive";
+import { PublisherWasNotFound } from "@publishers/domain/exceptions/PublisherWasNotFound";
+import type { Publisher } from "@publishers/domain/model/Publisher";
+import type { PublisherRepository } from "@publishers/domain/repository/PublisherRepository";
+import { PublisherId } from "@publishers/domain/value-objects/PublisherId";
+
+export class FindPublisherById {
+  constructor(private readonly repository: PublisherRepository) {}
+
+  async execute(id: string): Promise<Publisher> {
+    const publisher = await this.repository.findById(new PublisherId(id));
+
+    if (!publisher) throw new PublisherWasNotFound("Publisher was not found.");
+
+    if (!publisher.isActive())
+      throw new PublisherIsNotActive("Publisher is not active.");
+
+    return publisher;
+  }
+}
