5d93a4c694e35b968ec4e2e867d9463c95e55b6b
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 16:19:05 -0500

    chore: :art: Format Code

--- src/modules/publishers/domain/value-objects/PublisherUpdatedAt.ts
+++ src/modules/publishers/domain/value-objects/PublisherUpdatedAt.ts
@@ -7,8 +7,7 @@ export class PublisherUpdatedAt {
   }
 
   private validate(date: Date): void {
-    if (Number.isNaN(date.getTime()))
-      throw new Error("UpdatedAt date must be a valid Date instance");
+    if (Number.isNaN(date.getTime())) throw new Error("UpdatedAt date must be a valid Date instance");
 
     const now = new Date();
     if (date > now) throw new Error("UpdatedAt must be in the past");
