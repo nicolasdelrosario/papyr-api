bd62ec798050670e409bb0c6c429c7e0ebd802e6
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 15:07:44 -0500

    feat: :sparkles: Add AuthorId value object with UUID validation

---
+++ src/modules/authors/domain/value-objects/AuthorId.ts
@@ -0,0 +1,17 @@
+export class AuthorId {
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(uuid: string): void {
+    if (!uuid || typeof uuid !== "string")
+      throw new Error("UUID must be a valid string");
+
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    if (!uuidRegex.test(uuid)) throw new Error("Invalid UUID format");
+  }
+}
