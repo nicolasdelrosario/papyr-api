9cd7fb4e51c462e4744421233ffe9105b928d950
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 16:20:47 -0500

    feat: :sparkles: Add ListPublishers use case

---
+++ src/modules/publishers/application/use-cases/ListPublishers.ts
@@ -0,0 +1,12 @@
+import type { PublisherRepository } from "@publishers/domain/repository/PublisherRepository";
+import type { Publisher } from "@publishers/domain/model/Publisher";
+
+export class ListPublishers {
+  constructor(private readonly repository: PublisherRepository) {}
+
+  async execute(): Promise<Publisher[]> {
+    const publishers = await this.repository.list();
+
+    return publishers;
+  }
+}
