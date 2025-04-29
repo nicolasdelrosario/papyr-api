116d7783427552037d362ed20521e5a7ca9223b7
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-29 15:08:18 -0500

    feat: :sparkles: Add AuthorName value object with validation

---
+++ src/modules/authors/domain/value-objects/AuthorName.ts
@@ -0,0 +1,16 @@
+export class AuthorName {
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(name: string): void {
+    if (!name || typeof name !== "string")
+      throw new Error("Name must be a valid string");
+
+    if (name.trim().length < 3)
+      throw new Error("Name must be at least 3 non-space characters long");
+  }
+}
