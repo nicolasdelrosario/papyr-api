47ff2cd950d9b07e4e5bba74ee1e0f7aed56d93a
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-30 12:42:54 -0500

    feat: :sparkles: Add GenreName value object with validation

---
+++ src/modules/genres/domain/value-objects/GenreName.ts
@@ -0,0 +1,14 @@
+export class GenreName {
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(name: string): void {
+    if (!name || typeof name !== "string") throw new Error("Name must be a valid string");
+
+    if (name.trim().length < 3) throw new Error("Name must be at least 3 non-space characters long");
+  }
+}
