29064fecd6d0854bfe4f06739a05547294a5728e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-30 17:10:49 -0500

    feat: :sparkles: Add BookLanguage value object with validation

---
+++ src/modules/books/domain/value-objects/BookLanguage.ts
@@ -0,0 +1,14 @@
+export class BookLanguage {
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.validate(value);
+    this.value = value;
+  }
+
+  private validate(language: string): void {
+    if (!language || typeof language !== "string") throw new Error("Language must be a valid string");
+
+    if (language.trim().length < 2) throw new Error("Language name must be at least 2 non-whitespace characters long");
+  }
+}
