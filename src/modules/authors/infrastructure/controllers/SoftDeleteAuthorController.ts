4547f0fb02a400c932aa875180967e128450afac
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-01 16:51:44 -0500

    feat: :sparkles: Add SoftDeleteAuthorController implementation

---
+++ src/modules/authors/infrastructure/controllers/SoftDeleteAuthorController.ts
@@ -0,0 +1,35 @@
+import { AuthorIsNotActive } from "@authors/domain/exceptions/AuthorIsNotActive";
+import { AuthorWasNotFound } from "@authors/domain/exceptions/AuthorWasNotFound";
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import * as HttpStatusPhrases from "@core/common/httpStatusPhrases";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class SoftDeleteAuthorController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.authors.softDelete.execute(id);
+
+      return c.json(
+        {
+          data: null,
+          message: HttpStatusPhrases.OK,
+        },
+        HttpStatusCodes.OK,
+      );
+    } catch (error) {
+      if (error instanceof AuthorWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof AuthorIsNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.FORBIDDEN);
+
+      throw error;
+    }
+  }
+}
