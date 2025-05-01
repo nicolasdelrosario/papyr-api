84e51861da77aef0fe87709131622577233d6cb5
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-01 13:09:29 -0500

    feat: :sparkles: Add DeleteBook use case

---
+++ src/modules/books/application/use-cases/DeleteBook.ts
@@ -0,0 +1,19 @@
+import { BookIsActive } from "@books/domain/exceptions/BookIsActive";
+import { BookWasNotFound } from "@books/domain/exceptions/BookWasNotFound";
+import type { BookRepository } from "@books/domain/repository/BookRepository";
+import { BookId } from "@books/domain/value-objects/BookId";
+
+export class DeleteBook {
+  constructor(private readonly repository: BookRepository) {}
+
+  async execute(id: string) {
+    const bookId = new BookId(id);
+    const book = await this.repository.findById(bookId);
+
+    if (!book) throw new BookWasNotFound("Book was not found");
+
+    if (book.isActive()) throw new BookIsActive("Book is active, cannot be deleted");
+
+    await this.repository.delete(bookId);
+  }
+}
