e4201a5fb5e42a8c50aa37cb6bc920bfcabceaea
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-01 16:43:04 -0500

    feat: :sparkles: Add DeleteAuthorController for handling author deletion

---
+++ src/modules/authors/infrastructure/controllers/DeleteAuthorController.ts
@@ -0,0 +1,28 @@
+import { AuthorIsNotActive } from "@authors/domain/exceptions/AuthorIsNotActive";
+import { AuthorWasNotFound } from "@authors/domain/exceptions/AuthorWasNotFound";
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class DeleteAuthorController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.authors.delete.execute(id);
+
+      return c.json({ data: null, message: "Author deleted successfully" }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof AuthorWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof AuthorIsNotActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.CONFLICT);
+
+      throw error;
+    }
+  }
+}
