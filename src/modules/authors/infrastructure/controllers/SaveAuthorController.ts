c9dbf4d396eabe6b33dad56bca920f17cbc7ac17
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-01 16:56:15 -0500

    feat: :sparkles: Add SaveAuthorController for author management

---
+++ src/modules/authors/infrastructure/controllers/SaveAuthorController.ts
@@ -0,0 +1,29 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { EmailAlreadyInUse } from "@users/domain/exceptions/EmailAlreadyInUse";
+import { UserWasNotFound } from "@users/domain/exceptions/UserWasNotFound";
+import { UsernameAlreadyInUse } from "@users/domain/exceptions/UsernameAlreadyInUse";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class SaveAuthorController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const services = c.get("services");
+      const author = await c.req.json();
+
+      await services.authors.save.execute(author);
+
+      return c.json({ data: null, message: "Author was saved successfully" }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof EmailAlreadyInUse || error instanceof UsernameAlreadyInUse)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.CONFLICT);
+
+      if (error instanceof UserWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      throw error;
+    }
+  }
+}
