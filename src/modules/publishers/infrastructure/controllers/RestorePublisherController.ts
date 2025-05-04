1f4f16db2319c023281a05a590607bc6d2920b11
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-03 20:03:07 -0500

    feat: :sparkles: Add RestorePublisherController for publisher restoration

---
+++ src/modules/publishers/infrastructure/controllers/RestorePublisherController.ts
@@ -0,0 +1,34 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { PublisherIsActive } from "@publishers/domain/exceptions/PublisherIsActive";
+import { PublisherWasNotFound } from "@publishers/domain/exceptions/PublisherWasNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class RestorePublisherController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.publishers.restore.execute(id);
+
+      return c.json(
+        {
+          data: null,
+          message: "Publisher was restored successfully",
+        },
+        HttpStatusCodes.OK,
+      );
+    } catch (error) {
+      if (error instanceof PublisherWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof PublisherIsActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.CONFLICT);
+
+      throw error;
+    }
+  }
+}
