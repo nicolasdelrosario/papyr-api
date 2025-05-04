87121e65536d167c87e7e70fdd67ce43e01149a5
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-03 20:04:20 -0500

    feat: :sparkles: Add publisher routes to API registration

--- src/modules/core/infrastructure/hono/routes/registerRoutes.ts
+++ src/modules/core/infrastructure/hono/routes/registerRoutes.ts
@@ -3,9 +3,10 @@ import type { Hono } from "hono";
 
 import * as authRoutes from "@auth/infrastructure/routes/honoAuthRoutes";
 import * as authorRoutes from "@authors/infrastructure/routes/honoAuthorRoutes";
+import * as publisherRoutes from "@publishers/infrastructure/routes/honoPublisherRoutes";
 import * as userRoutes from "@users/infrastructure/routes/honoUserRoutes";
 
-const routes = [authRoutes, userRoutes, authorRoutes];
+const routes = [authRoutes, userRoutes, authorRoutes, publisherRoutes];
 
 export function registerRoutes(app: Hono<App>): void {
   for (const route of routes) {
