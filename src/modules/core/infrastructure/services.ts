8e82d163df0210613f11721d65c26b50fb8606f3
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-03 20:01:13 -0500

    feat: :sparkles: Add publisher service and repository imports and wiring

--- src/modules/core/infrastructure/services.ts
+++ src/modules/core/infrastructure/services.ts
@@ -7,6 +7,14 @@ import { RestoreAuthor } from "@/modules/authors/application/use-cases/RestoreAu
 import { SaveAuthor } from "@/modules/authors/application/use-cases/SaveAuthor";
 import { SoftDeleteAuthor } from "@/modules/authors/application/use-cases/SoftDeleteAuthor";
 import type { AuthorRepository } from "@/modules/authors/domain/repository/AuthorRepository";
+import { DeletePublisher } from "@/modules/publishers/application/use-cases/DeletePublisher";
+import { FindPublisherById } from "@/modules/publishers/application/use-cases/FindPublisherById";
+import { ListPublishers } from "@/modules/publishers/application/use-cases/ListPublishers";
+import { RestorePublisher } from "@/modules/publishers/application/use-cases/RestorePublisher";
+import { SavePublisher } from "@/modules/publishers/application/use-cases/SavePublisher";
+import { SoftDeletePublisher } from "@/modules/publishers/application/use-cases/SoftDeletePublisher";
+import type { PublisherRepository } from "@/modules/publishers/domain/repository/PublisherRepository";
+import { D1PublisherRepository } from "@/modules/publishers/infrastructure/repository/D1PublisherRepository";
 import { DeleteUser } from "@/modules/users/application/use-cases/DeleteUser";
 import { FindUserByEmail } from "@/modules/users/application/use-cases/FindUserByEmail";
 import { FindUserById } from "@/modules/users/application/use-cases/FindUserById";
@@ -24,6 +32,7 @@ export const services = (db: D1Database, jwtSecret: string) => {
   // repositories
   const userRepository: UserRepository = new D1UserRepository(db);
   const authorRepository: AuthorRepository = new D1AuthorRepository(db);
+  const publisherRepository: PublisherRepository = new D1PublisherRepository(db);
 
   // services
   const encryptionService = new BcryptEncryptionService();
@@ -50,6 +59,14 @@ export const services = (db: D1Database, jwtSecret: string) => {
       delete: new DeleteAuthor(authorRepository),
       restore: new RestoreAuthor(authorRepository),
       softDelete: new SoftDeleteAuthor(authorRepository),
+    },
+    publishers: {
+      list: new ListPublishers(publisherRepository),
+      findById: new FindPublisherById(publisherRepository),
+      save: new SavePublisher(publisherRepository),
+      delete: new DeletePublisher(publisherRepository),
+      restore: new RestorePublisher(publisherRepository),
+      softDelete: new SoftDeletePublisher(publisherRepository),
     },
   };
 };
