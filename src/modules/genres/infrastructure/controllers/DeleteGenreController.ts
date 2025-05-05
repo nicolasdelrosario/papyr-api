080537dbec4b83e1d5f8858567b08c36569f986c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-04 23:10:07 -0500

    feat: :sparkles: Add DeleteGenreController

---
+++ src/modules/genres/infrastructure/controllers/DeleteGenreController.ts
@@ -0,0 +1,28 @@
+import * as HttpStatusCodes from "@core/common/httpStatusCodes";
+import type { Controller, ControllerResponse } from "@core/infrastructure/Controller";
+import type { App } from "@core/infrastructure/hono/types/App";
+import { GenreIsActive } from "@genres/domain/exceptions/GenreIsActive";
+import { GenreWasNotFound } from "@genres/domain/exceptions/GenreWasNotFound";
+import type { Context, TypedResponse } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
+
+export class DeleteGenreController implements Controller {
+  async handle(c: Context<App>): Promise<Response & TypedResponse<ControllerResponse, StatusCode, "json">> {
+    try {
+      const id = c.req.param("id");
+      const services = c.get("services");
+
+      await services.genres.delete.execute(id);
+
+      return c.json({ data: null, message: "Genre was deleted successfully" }, HttpStatusCodes.OK);
+    } catch (error) {
+      if (error instanceof GenreWasNotFound)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.NOT_FOUND);
+
+      if (error instanceof GenreIsActive)
+        return c.json({ data: null, message: error.message }, HttpStatusCodes.CONFLICT);
+
+      throw error;
+    }
+  }
+}
