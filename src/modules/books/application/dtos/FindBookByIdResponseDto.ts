87aebd95671472a6a3ffdf95b3ffa974a0caf410
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-05 18:34:11 -0500

    feat: :sparkles: Add FindBookByIdResponseDto for book retrieval response structure

---
+++ src/modules/books/application/dtos/FindBookByIdResponseDto.ts
@@ -0,0 +1,25 @@
+export type FindBookByIdResponseDto = {
+  book: {
+    id: string;
+    authorId: string;
+    publisherId: string;
+    title: string;
+    description: string | null;
+    isbn: string | null;
+    publicationDate: Date;
+    coverUrl: string | null;
+    pages: number;
+    language: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date | null;
+    author: {
+      id: string;
+      name: string;
+    };
+    publisher: {
+      id: string;
+      name: string;
+    };
+  };
+}; 
