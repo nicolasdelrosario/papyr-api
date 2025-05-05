1365f05d80eaa4c5a6b1da7a0a21323995cd5050
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-05 15:49:01 -0500

    feat: :sparkles: Add book routes to Hono application

--- src/modules/core/infrastructure/hono/routes/registerRoutes.ts
+++ src/modules/core/infrastructure/hono/routes/registerRoutes.ts
@@ -3,11 +3,12 @@ import type { Hono } from "hono";
 
 import * as authRoutes from "@auth/infrastructure/routes/honoAuthRoutes";
 import * as authorRoutes from "@authors/infrastructure/routes/honoAuthorRoutes";
+import * as bookRoutes from "@books/infrastructure/routes/honoBookRoutes";
 import * as genresRoutes from "@genres/infrastructure/routes/honoGenreRoutes";
 import * as publisherRoutes from "@publishers/infrastructure/routes/honoPublisherRoutes";
 import * as userRoutes from "@users/infrastructure/routes/honoUserRoutes";
 
-const routes = [authRoutes, userRoutes, authorRoutes, publisherRoutes, genresRoutes];
+const routes = [authRoutes, userRoutes, authorRoutes, publisherRoutes, genresRoutes, bookRoutes];
 
 export function registerRoutes(app: Hono<App>): void {
   for (const route of routes) {
