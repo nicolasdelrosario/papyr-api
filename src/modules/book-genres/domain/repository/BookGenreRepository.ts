9338994fa8461adb5c4377c495a7c6db3b8f5bdb
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-06 15:04:25 -0500

    fix: :bug: Fix list method signature in BookGenreRepository interface

--- src/modules/book-genres/domain/repository/BookGenreRepository.ts
+++ src/modules/book-genres/domain/repository/BookGenreRepository.ts
@@ -6,7 +6,7 @@ import type { BookId } from "@books/domain/value-objects/BookId";
 export interface BookGenreRepository {
   listBooksByGenreId(genreId: GenreId): Promise<BookGenre[]>;
   listGenresByBookId(bookId: BookId): Promise<BookGenre[]>;
-  list: Promise<BookGenre[]>;
+  list(): Promise<BookGenre[]>;
   findById(id: BookGenreId): Promise<BookGenre | null>;
   save(bookGenre: BookGenre): Promise<void>;
   delete(id: BookGenreId): Promise<void>;
