9924cd40da5c1818b407a661de13e358643f8e5d
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-06 14:59:38 -0500

    feat: :sparkles: Add BookGenre DTOs for data transfer objects

---
+++ src/modules/book-genres/application/dtos/BookGenreDto.ts
@@ -0,0 +1,8 @@
+export interface BookGenreDTO {
+  id: string;
+  book_id: string;
+  genre_id: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+} 
---
+++ src/modules/book-genres/application/dtos/SaveBookGenreDto.ts
@@ -0,0 +1,5 @@
+export interface SaveBookGenreDTO {
+  id: string;
+  bookId: string;
+  genreId: string;
+}
