2f2a386f3d6b822fad5cb5b16f266574ab54ed5f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-05-07 14:00:17 -0500

    refactor: :recycle: Update BookGenreRepository to return Book and Genre types instead of BookGenre for improved data clarity

--- src/modules/book-genres/domain/repository/BookGenreRepository.ts
+++ src/modules/book-genres/domain/repository/BookGenreRepository.ts
@@ -1,11 +1,13 @@
+import type { Book } from "@/modules/books/domain/model/Book";
+import type { Genre } from "@/modules/genres/domain/model/Genre";
 import type { GenreId } from "@/modules/genres/domain/value-objects/GenreId";
 import type { BookGenre } from "@bookGenres/domain/model/BookGenre";
 import type { BookGenreId } from "@bookGenres/domain/value-objects/BookGenreId";
 import type { BookId } from "@books/domain/value-objects/BookId";
 
 export interface BookGenreRepository {
-  listBooksByGenreId(genreId: GenreId): Promise<BookGenre[]>;
-  listGenresByBookId(bookId: BookId): Promise<BookGenre[]>;
+  listBooksByGenreId(genreId: GenreId): Promise<Book[]>;
+  listGenresByBookId(bookId: BookId): Promise<Genre[]>;
   list(): Promise<BookGenre[]>;
   findById(id: BookGenreId): Promise<BookGenre | null>;
   save(bookGenre: BookGenre): Promise<void>;
